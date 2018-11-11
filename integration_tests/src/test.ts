6f75196fd101e8d90762312a515e68fe3e8753e1
Roberts Kolosovs <roberts.kolosovs@pwc.com>
2018-11-11 23:09:05 +0100

    Basic ts qunit testfile for testing Docker and compose

---
+++ integration_tests/src/test.ts
@@ -0,0 +1,23 @@
+/*Copyright 2018 Janos Klieber, Roberts Kolosovs, Peter Spieler
+This file is part of Phoenixclient.
+
+Phoenixclient is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Phoenixclient is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Phoenixclient.  If not, see <http://www.gnu.org/licenses/>.*/
+
+import {test, module} from "../node_modules/qunit";
+
+module("Test module", function () {
+    test("My first test", function (t: any) {
+        t.strictEqual(2+2, 4, "2+2 should be 4");
+    });
+}

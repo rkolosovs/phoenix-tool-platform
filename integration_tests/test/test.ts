7a7fa22369a972fdef6b4058ab711dd0fcdb8c1b
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2018-12-08 17:05:00 +0100

    Wired client source files to the integration tests

--- integration_tests/test/test.ts
+++ integration_tests/test/test.ts
@@ -16,8 +16,21 @@ along with Phoenixclient.  If not, see <http://www.gnu.org/licenses/>.*/
 
 import {test, module} from "../node_modules/qunit";
 
+import {Realm} from "../../phoenix-tool-client/src/model/realm";
+import {FootArmy} from "../../phoenix-tool-client/src/model/armies/footArmy";
+import {FieldType} from "../../phoenix-tool-client/src/model/map/field";
+
 module("Test module", function () {
     test("My first test", function (t: any) {
         t.strictEqual(2+2, 4, "2+2 should be 4");
+    });
+    test("footArmyConstructor", function (t: any) {
+        const usa = new Realm(
+            "Unabhängige Stämme Assimilans",
+            "usa", "000,000,000",
+            FieldType.DESERT,
+            true);
+        const footArmy = new FootArmy(111, usa, 1000, 1, 0, 0, 0, [0, 0], 0, 0, false);
+        t.strictEqual(footArmy.canHaveCatapults(), true, "non guard armies can have catapults.");
     });
 });

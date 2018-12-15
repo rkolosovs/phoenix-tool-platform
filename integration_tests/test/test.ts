d3ae02f31e148d1a1377dbc62887305a632c09e2
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2018-12-16 00:53:19 +0100

    Getting integration test to wait until the server has spun up

--- integration_tests/test/test.ts
+++ integration_tests/test/test.ts
@@ -14,19 +14,27 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Phoenixclient.  If not, see <http://www.gnu.org/licenses/>.*/
 
-import {test, module} from "../node_modules/qunit";
+import {test, module, config} from "../node_modules/qunit";
 import {loadCurrentTurn} from "../../phoenix-tool-client/src/serverInteraction/loadingFunctions";
 import {TurnStatus} from "../../phoenix-tool-client/src/gameState/gameState";
 
 module("Test module", function () {
     test("currentTurnLoading", async function (t: any) {
-        let actualResult = loadCurrentTurn();
+        config.testTimeout = 1000;
+        t.async();
+        let actualResultPromise = loadCurrentTurn();
         let expectedResult = {
             turn: 1,
-            realm: 'sl',
+            realm: null,
             status: TurnStatus.STARTED
         };
-        t.strictEqual(await actualResult, expectedResult, "should get current turn.");
-        t.async()
+        t.strictEqual(await actualResultPromise, expectedResult, "should get current turn.");
+        // actualResultPromise.then((actualResult) => {
+        //         t.strictEqual(actualResult, expectedResult, "should get current turn.");
+        //     }
+        // ).catch((error) => {
+        //         console.error("Promise rejected: ", error);
+        //     }
+        // );
     });
 });

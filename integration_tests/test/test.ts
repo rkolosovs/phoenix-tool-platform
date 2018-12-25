ee904bda320e3a21c51289adb52b98d68b5cb5c3
Roberts Kolosovs <roberts.kolosovs@stud.tu-darmstadt.de>
2018-12-25 16:51:04 +0100

    Test for realm loading

--- integration_tests/test/test.ts
+++ integration_tests/test/test.ts
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with Phoenixclient.  If not, see <http://www.gnu.org/licenses/>.*/
 
 import {test, module, config} from "../node_modules/qunit";
-import {loadCurrentTurn} from "../../phoenix-tool-client/src/serverInteraction/loadingFunctions";
+import {loadCurrentTurn, loadRealms} from "../../phoenix-tool-client/src/serverInteraction/loadingFunctions";
 import {TurnStatus} from "../../phoenix-tool-client/src/gameState/gameState";
 
 module("Test module", function () {
@@ -29,6 +29,43 @@ module("Test module", function () {
             realm: null
         };
         t.deepEqual(await actualResultPromise, expectedResult, "should get current turn.");
+        done();
+    });
+    test("realmsLoading", async function (t: any) {
+        config.testTimeout = 10000;
+        let done = t.async();
+        let actualResultPromise = loadRealms();
+        let expectedResult = [
+            {
+                tag: "rot",
+                name: "Rot",
+                color: "255,000,000",
+                homeTurf: 1,
+                active: true
+            },
+            {
+                tag: "grn",
+                name: "Gruen",
+                color: "000,255,000",
+                homeTurf: 1,
+                active: true
+            },
+            {
+                tag: "blu",
+                name: "Blau",
+                color: "000,000,255",
+                homeTurf: 1,
+                active: true
+            },
+            {
+                tag: "pnk",
+                name: "Pink",
+                color: "255,105,180",
+                homeTurf: 1,
+                active: true
+            }
+        ];
+        t.deepEqual(await actualResultPromise, expectedResult, "should get realms.");
         done();
     });
 });
